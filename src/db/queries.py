2fb0a10d82c63a8f5191d74d7d6d62f5c0630286
Your Real Name <maccutler@protonmail.com>
2026-05-06 21:21:55 -0700

    Bugfix

--- src/db/queries.py
+++ src/db/queries.py
@@ -6,7 +6,7 @@ load_dotenv()
 supabase = create_client(os.environ["SUPABASE_URL"], os.environ["SUPABASE_KEY"])
 
 def get_products_for_banner(banner_id: int) -> list[dict]:
-    response = supabase.table("product_aliases") \
+    response = supabase.schema("public").table("product_aliases") \
         .select("product_id, banner_sku") \
         .eq("banner_id", banner_id) \
         .execute()
